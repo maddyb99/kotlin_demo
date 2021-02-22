36b50601b42ca46f8fec44b233e574c3c5bae233
maddyb99 <43785086+maddyb99@users.noreply.github.com>
2021-02-22 22:40:19 +0530

    minor changes

--- app/src/main/java/tech/maddybcorp/myapplication/MainActivity.kt
+++ app/src/main/java/tech/maddybcorp/myapplication/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        title="Test"
         // Example of a call to a native method
         findViewById<TextView>(R.id.sample_text).text = stringFromJNI()
         val textView=findViewById<TextView>(R.id.textView_progress)
